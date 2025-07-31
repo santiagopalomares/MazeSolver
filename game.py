9d1a126beef80c5f3a5b30cff68f52c0ff3d3cdd
santiago <spaloma.v1@gmail.com>
2025-07-30 23:53:40 -0700

    feat: game class

--- game.py
+++ game.py
@@ -7,7 +7,26 @@ class Game:
         self.maze = maze
         self.solver = solver
         self.player = Player(maze.start)
-        self.path = []  # TODO: Store the solution path
+        self.path = solver.solve(maze)
 
     def run(self):
-        pass  # TODO: Main game loop: display, get input, move player, check win 
+        while True:
+            self.maze.display(self.player.position, self.path)
+            
+            if self.player.position == self.maze.end:
+                print("Congratulations! You've reached the end!")
+                break
+            
+            move = input("Enter move (w/a/s/d or q to quit): ").lower()
+            
+            if move == 'q':
+                print("Game ended.")
+                break
+            elif move == 'w':
+                self.player.move('up', self.maze)
+            elif move == 's':
+                self.player.move('down', self.maze)
+            elif move == 'a':
+                self.player.move('left', self.maze)
+            elif move == 'd':
+                self.player.move('right', self.maze) 
