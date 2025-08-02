d9b50532085d5512ca7a72c426ca956b85986501
santiago <spaloma.v1@gmail.com>
2025-08-01 23:15:20 -0700

    feat: basic run possible, solver implemented

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from solver import DFSMazeSolver
 from game import Game
 
 if __name__ == "__main__":
+    # Create a more interesting maze layout
     grid = [
-        ['S', 0, 1, 0, 'E'],
-        [1, 0, 1, 0, 1],
-        [0, 0, 0, 0, 0],
-    ]  # TODO: Replace with dynamic input or file loading
+        ['S', 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 'E']
+    ]
+    
     maze = Maze(grid)
     solver = DFSMazeSolver()
     game = Game(maze, solver)
-    game.run()  # TODO: Implement game loop 
+    game.run() 
--- solver.py
+++ solver.py
@@ -4,8 +4,31 @@ from maze import Maze
 class MazeSolver(ABC):
     @abstractmethod
     def solve(self, maze: Maze):
-        pass  # TODO: Return a list of positions from start to end
+        pass
 
 class DFSMazeSolver(MazeSolver):
     def solve(self, maze: Maze):
-        pass  # TODO: Implement DFS algorithm 
+        """Solve the maze using Depth-First Search algorithm."""
+        visited = set()
+        path = []
+        
+        def dfs(current_pos):
+            if current_pos == maze.end:
+                return True
+            
+            visited.add(current_pos)
+            
+            for neighbor in maze.get_neighbors(current_pos):
+                if neighbor not in visited:
+                    if dfs(neighbor):
+                        path.append(neighbor)
+                        return True
+            
+            return False
+        
+        # Start DFS from the start position
+        if dfs(maze.start):
+            path.append(maze.start)
+            return path[::-1]  # Reverse to get start to end order
+        else:
+            return []  # No path found 
